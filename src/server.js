e22690c8251973262d44aa84ca913b5668433e3e
DatDoubleD <buonqa05@gmail.com>
2021-07-24 11:20:30 +0700

    fix server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const morgan = require('morgan');
+//const morgan = require('morgan');
 require('dotenv').config({ path: `${__dirname}/setup.env` });
 const app = express();
 const methodOverride = require('method-override');
