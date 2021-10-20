0d17026c0f3b34acc6b42cbfdf1b6f2b17e2ac1f
DatDoan20 <doanducdat271020@gmail.com>
2021-10-20 16:37:29 +0700

    fix database and package-lock

--- src/config/database.js
+++ src/config/database.js
@@ -3,18 +3,11 @@ const mongoose = require('mongoose');
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 async function connect() {
 	try {
-		await mongoose
-			.connect(DB, {
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-				useCreateIndex: true,
-			})
-			.then((connectInfo) => {
-				console.log(`${'-'.repeat(55)}✨✨✨ DB connect Successfully!!✨✨✨`);
-			});
+		await mongoose.connect(DB, {}).then((connectInfo) => {
+			console.log(`${'-'.repeat(55)}✨✨✨ DB connect Successfully!!✨✨✨`);
+		});
 	} catch (error) {
 		console.log('connect failure ');
 	}
 }
-mongoose.set('useFindAndModify', false);
 module.exports = { connect };
